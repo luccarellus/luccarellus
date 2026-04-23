3b4e29878683fe7756d6d6ac67c24603512843ce
Lucas Barbosa <lucasestvr@icloud.com>
2026-04-23 11:20:45 -0300

    Delete neva-pro directory

--- neva-pro/backend/src/modules/materials/materials.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MaterialsService } from './materials.service';
-
-@Module({
-  providers: [MaterialsService],
-  exports: [MaterialsService],
-})
-export class MaterialsModule {}
