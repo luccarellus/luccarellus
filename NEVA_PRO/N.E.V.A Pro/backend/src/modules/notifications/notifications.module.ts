cfbb163da6c4944042d60e47fa0c16e7d9fc37f1
Lucas Barbosa <lucasestvr@icloud.com>
2026-04-23 14:58:19 -0300

    Delete NEVA_PRO directory

--- NEVA_PRO/N.E.V.A Pro/backend/src/modules/notifications/notifications.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MuralService } from './mural.service';
-
-@Module({
-  providers: [MuralService],
-  exports: [MuralService],
-})
-export class NotificationModule {}
